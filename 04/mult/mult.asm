73a13bc845ac59d9fb316b8d4e38e58d8f08ef17
jrscott-goggles <jrscott@smu.edu>
2018-03-21 12:06:03 -0500

    04: Mult done

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+	@R1 //set counter (R3) = R1
+	D=M
+	@R3
+	M=D
+	@R2 //set R2 = 0
+	M=0
+(MUL_LOOP)
+	@R1
+	D=M
+	@END
+	D;JEQ
+	
+	@R0 //add R0 to R2
+	D=M
+	@R2
+	M=D+M
+
+	@R3 //decrement counter
+	MD=M-1
+	@MUL_LOOP
+	D;JNE
+
+(END)
+	@END
+	0;JMP
